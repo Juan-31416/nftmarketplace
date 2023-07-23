f03d31ea67455338a19c5187cd7980370ab69aed
Juan-31416 <juan.py.tejeiro@outlook.com>
2023-07-23 13:09:36 +0200

    Added Service section

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 //INTERNAL IMPORT
 import Style from '../styles/Index.module.css';
 import { HeroSection } from '../components/Components_index';
+import {Service} from '../components/Components_index';
 
 const Home = () => {
   return <div className={Style.homePage}>
     <HeroSection/>
+    <Service/>
   </div>;
 };
 
