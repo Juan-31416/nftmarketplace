7b096e8f8381752b59ec661c04538a9f76b8833f
Juan-31416 <juan-31416@users.noreply.github.com>
2023-08-12 13:29:25 +0200

    New BigNFTSlider folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '@/styles/globals.css';
 
 //INTERNAL IMPORT
-import {NavBar, Footer, HeroSection, Service} from "../components/Components_index";
+import {
+    NavBar, 
+    Footer, 
+    HeroSection, 
+    Service,
+    BigNFTSlider
+} from "../components/Components_index";
 
 const MyApp = ({ Component, pageProps })=> (
     <div>
@@ -10,6 +16,7 @@ const MyApp = ({ Component, pageProps })=> (
         <Footer />
         <HeroSection />
         <Service />
+        <BigNFTSlider />
     </div>
 );
 
