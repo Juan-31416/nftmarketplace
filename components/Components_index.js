7a5803c352a6127e45d34655c5a8b42c29a93bc7
Juan-31416 <juan.py.tejeiro@outlook.com>
2023-09-13 07:22:22 +0200

    Solved some issues. Yet to find some.

--- components/Components_index.js
+++ components/Components_index.js
@@ -4,7 +4,7 @@ import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
-import Service from "./Service/Service";
+import Subscribe from "./Subscribe/Subscribe";
 import Title from "./Title/Title";
 import Category from "./Category/Category";
 import Filter from "./Filter/Filter";
@@ -18,6 +18,7 @@ export{
     HeroSection, 
     Service, 
     BigNFTSlider, 
+    Subscribe,
     Title,
     Category,
     Filter,
